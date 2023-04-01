c7c605a4d393d610acf069cd272eb5205a8b3f2f
richard lazado <richardlazado614@gmail.com>
2023-04-01 22:24:23 +0800

    Add files via upload

---
+++ profile/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>R | Lazado</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel = "icon" href = "https://img.icons8.com/avantgarde/256/r.png" type = "image/x-icon">
+</head>
+
+<body>
+
+	<main>
+		
+		<div class="card">
+
+			<div class="card1">
+				
+
+				<img src="./img/5.png" alt="Picture">
+
+					<div class="profilet">
+						<h1 style="font-family: 'Roboto', sans-serif;">Richard Lazado</h1>
+						<span class="br"></span>
+						<hr color="#c4c4c4">
+						<span class="br"></span>
+						<p style="text-align:justify; font-family: 'Roboto', sans-serif;" >I'm a third-year college student at Trimex Colleges of Biñan,Laguna 20 years of age. I'm taking Information Technologies with a focus on web development, web design, and app development.</p>
+				
+					</div>
+
+
+					<div class="mainbody">
+
+							<div class="skills">
+					
+									<h3>SKILLS</h3>
+									<span class="br"></span>
+
+								<ul>
+									<li>C#</li>
+									<li>Java</li>
+									<li>HTML/CSS</li>
+									<li>C++</li>
+									<li>Php</li>
+								</ul>
+
+							</div>
+
+							<div class="line"></div>
+
+							<div class="hobbies">
+
+								<h3>HOBBIES</h3>
+								<span class="br"></span>
+
+								<ul>
+									<li>Games</li>
+									<li>Sleep</li>
+									<li>Coding</li>
+									<li>Movies</li>
+									<li>Guitar</li>
+								</ul>
+							</div>
+
+					</div>
+
+						<div class="socmed">
+							<a href="https://www.facebook.com/richard.lazado.925/"><i class="fa-brands fa-facebook"></i></a>
+							<a href="https://www.messenger.com/t/100004475047202/"><i class="fa-brands fa-facebook-messenger"></i></a>
+							<a href="https://www.instagram.com/richardlazado/"><i class="fa-brands fa-instagram"></i></a>
+							<a href="https://www.tiktok.com/@yshkaira"><i class="fa-brands fa-tiktok"></i></a>
+							<a href="https://myaccount.google.com/?hl=en&utm_source=OGB&utm_medium=act0"><i class="fa-solid fa-envelope"></i></a>
+						</div>
+
+			</div>
+		</div>		
+		
+	</main>
+
+</body>
+</html>
